0e5cacfdcddc714628244115c3164b32769904f5
stoney98 <stoney98@qq.com>
2023-04-09 22:16:20 +0800

    add testfile file

---
+++ testfile/conditiondaiMa.lua
@@ -0,0 +1,25 @@
+LogwLuaCore: LUA:[DumpTable.lua:143] table: 000001A02E302BE0 = {
+    [6028] = {
+        ["isDrop"] = false,
+        ["failConditions"] = {
+        },
+        ["succConditions"] = {
+            [1] = table: 000001A0658B8980
+        },
+        ["isNewTask"] = true,
+        ["counter"] = {
+        },
+        ["Destroy"] = function: 000001A014216790,
+        ["IsValid"] = function: 000001A0523CACA0,
+        ["isFreezed"] = false,
+        ["reserved"] = "",
+        ["selected_branch"] = 0,
+        ["recvCount"] = 0,
+        ["state"] = 1,
+        ["finishTime"] = 0,
+        ["success"] = false,
+        ["finishCount"] = 0,
+        ["taskId"] = 6028,
+        ["lastClearTime"] = 0,
+        ["recvTime"] = 0,
+    },
